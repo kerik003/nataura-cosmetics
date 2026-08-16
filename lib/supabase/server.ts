7fdfb7439b3c9311683fc423125267eb90fe2cf4
kerik003 <danilokernitskiy980@gmail.com>
2026-08-17 00:53:04 +0300

    Fix CookieOptions types

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -11,10 +11,10 @@ export function createClient() {
         get(name: string) {
           return cookieStore.get(name)?.value;
         },
-        set(name: string, value: string, options) {
+        set(name: string, value: string, options: CookieOptions) {
           cookieStore.set({ name, value, ...options });
         },
-        remove(name: string, options) {
+        remove(name: string, options: CookieOptions) {
           cookieStore.set({ name, value: "", ...options });
         },
       },
